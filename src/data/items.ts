607c8fa66f6848158798983ec5d4cf95b89a6707
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-27 00:04:54 -0300

    criando o data e os types

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022,8,26), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022,8,26), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2022,8,16), category: 'rent', title: 'Aluguel Apt', value: 530 },
+    { date: new Date(2022,9,16), category: 'salary', title: 'Salário', value: 3000 },
+];
